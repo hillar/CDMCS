8dea8590948451d2d242ef0a78693d6783498b1e
Hillar Aarelaid <hillar@aarelaid.net>
2016-02-02 22:37:01 +0200

    added scirius first run

--- suricata/day_intro/SSEE-easybutton-singlehost.sh
+++ suricata/day_intro/SSEE-easybutton-singlehost.sh
@@ -29,12 +29,12 @@ ethtool -K eth0 tx off sg off gro off gso off lro off tso off
 # see https://github.com/StamusNetworks/scirius#installation-and-setup
 
 DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y wget python-pip python-dev git gcc
-echo "deb http://packages.stamus-networks.com/debian/ jessie main" >> /etc/apt/sources.list.d/stamus-packages.list
+#echo "deb http://packages.stamus-networks.com/debian/ jessie main" >> /etc/apt/sources.list.d/stamus-packages.list
 cd /tmp/
-wget -O stamus.key -q http://packages.stamus-networks.com/packages.stamus-networks.com.gpg.key
-apt-key add stamus.key
-apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y suricata
+#wget -O stamus.key -q http://packages.stamus-networks.com/packages.stamus-networks.com.gpg.key
+#apt-key add stamus.key
+#apt-get update
+#DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y suricata
 #wget https://github.com/StamusNetworks/scirius/archive/scirius-${VERSION}.tar.gz
 wget https://github.com/StamusNetworks/scirius/archive/master.tar.gz
 mkdir -p /opt/selks/sciriusdata
@@ -51,32 +51,62 @@ wget -q https://raw.githubusercontent.com/StamusNetworks/Amsterdam/master/src/do
 chmod ugo+x /opt/selks/bin/scirius.sh
 pip install -U six
 pip install urllib3 --upgrade
+#/opt/selks/bin/scirius.sh
+cd /opt/selks/scirius/
+echo "no" | python manage.py syncdb
+python manage.py makemigrations
+python manage.py migrate
+#python manage.py loaddata /opt/selks/scirius/scirius.json
+python manage.py createcachetable my_cache_table
+python manage.py addsource "ETOpen Ruleset" https://rules.emergingthreats.net/open/suricata-2.0.7/emerging.rules.tar.gz http sigs
+python manage.py addsource "SSLBL abuse.ch" https://sslbl.abuse.ch/blacklist/sslblacklist.rules http sig
+python manage.py defaultruleset "Default SELKS ruleset"
+python manage.py disablecategory "Default SELKS ruleset" stream-events
+python manage.py addsuricata $(hostname) "Suricata on SELKS" /etc/suricata/rules "Default SELKS ruleset"
+python manage.py updatesuricata
+suricata -T -c /etc/suricata/suricata.yaml
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@localhost', 'password')" | python manage.py shell
+python manage.py runserver 0.0.0.0:8000 > /var/log/scirius.log 2>&1 &
+service suricata start
+
 
 
 #elastic
 apt-get install -y openjdk-7-jre-headless
 wget -q https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/deb/elasticsearch/2.1.1/elasticsearch-2.1.1.deb
 sudo dpkg -i elasticsearch-2.1.1.deb
-service elasticsearch stop
+#service elasticsearch stop
 /usr/share/elasticsearch/bin/plugin install mobz/elasticsearch-head
 #/usr/share/elasticsearch/bin/plugin install elasticsearch/elasticsearch-mapper-attachments/3.1.1
 /usr/share/elasticsearch/bin/plugin install delete-by-query
-
-#kibana
-cd /tmp
-wget -q https://download.elastic.co/kibana/kibana/kibana-4.3.1-linux-x64.tar.gz
-cd /opt/selks
-tar -xzf /tmp/kibana-4.3.1-linux-x64.tar.gz
-ln -sf /opt/selks/kibana-4.3.1-linux-x64 /opt/selks/kibana
-/opt/selks/kibana/bin/kibana plugin -i kibana/timelion
+echo "network.host: 0.0.0.0" >> /etc/elasticsearch/elasticsearch.yml
+service elasticsearch restart
 
 
 # Markus, can you replace logststash with rsyslog
 # logstash
-wget https://download.elastic.co/logstash/logstash/packages/debian/logstash_2.1.1-1_all.deb
-dpkg -i logstash_2.1.1-1_all.deb
+#wget https://download.elastic.co/logstash/logstash/packages/debian/logstash_2.1.1-1_all.deb
+#dpkg -i logstash_2.1.1-1_all.deb
+cd /tmp
+wget -q https://download.elastic.co/logstash/logstash/logstash-all-plugins-2.1.0.tar.gz
+cd /opt
+tar -xzf /tmp/logstash-all-plugins-2.1.0.tar.gz
 #stealing amsterdam losgstash conf
-wget https://github.com/StamusNetworks/Amsterdam/blob/master/src/config/logstash/logstash.conf
+mkdir -p /etc/logstash
+wget -q https://raw.githubusercontent.com/StamusNetworks/Amsterdam/master/src/config/logstash/logstash.conf -O /etc/logstash/logstash.conf
+echo "127.0.0.1 elasticsearch" >> /etc/hosts
+cd /opt/logstash-2.1.0/bin/
+./logstash -f /etc/logstash/logstash.conf > /var/log/scirius.log 2>&1 &
+
+#kibana
+cd /tmp
+wget -q https://download.elastic.co/kibana/kibana/kibana-4.3.1-linux-x64.tar.gz
+cd /opt/
+tar -xzf /tmp/kibana-4.3.1-linux-x64.tar.gz
+ln -sf /opt/kibana-4.3.1-linux-x64 /opt/kibana
+/opt/kibana/bin/kibana plugin -i kibana/timelion
+#chown -R kibana.kibana /opt/kibana/optimize/
+/opt/kibana/bin/kibana > /var/log/kibana.log 2>&1 &
 
 
 #evebox
@@ -85,4 +115,10 @@ cd /opt/
 wget https://bintray.com/artifact/download/jasonish/evebox/evebox-linux-amd64.zip
 unzip evebox-linux-amd64.zip
 ./evebox-linux-amd64/evebox --version
-# ./evebox-linux-amd64/evebox
+./evebox-linux-amd64/evebox
+echo "http.cors.enabled: true" >> /etc/elasticsearch/elasticsearch.yml
+echo "http.cors.allow-origin: \"/.*/\"" >> /etc/elasticsearch/elasticsearch.yml
+service elasticsearch restart
+./evebox-linux-amd64/evebox > /var/log/evebox.log 2>&1 &
+
+grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' /etc/suricata/rules/scirius.rules|rev|sort|uniq|rev|while read i; do wget -q -T 1 -t 1 $i; done >/dev/null &
