f1f2e5fb24fc351c0331add2861ca03abc90853c
Hillar Aarelaid <hillar@aarelaid.net>
2016-02-08 01:34:20 +0200

    added sure install

--- common/vagrant/bb/scripts/install-suri.sh
+++ common/vagrant/bb/scripts/install-suri.sh
@@ -1,2 +1,28 @@
 #!/bin/bash
-echo "$0 got params: $1 $2 $3 $4"
+#
+# this script
+# 0) sets gro,gso,lro,tso off
+# 1) addp oisf ppa
+# 2) installs suricata
+# 3) sets suricata conf as amsterdam
+# 4)
+
+
+if [ "$(id -u)" != "0" ]; then
+   echo "ERROR - This script must be run as root" 1>&2
+   exit 1
+fi
+
+ETH=$1
+SCIRIUS=$2
+
+ethtool -K $ETH tx off sg off gro off gso off lro off tso off
+
+#suricata
+add-apt-repository -y ppa:oisf/suricata-stable
+apt-get update
+apt-get -y install suricata
+service suricata stop
+#stealing amsterdam suricata conf
+wget -q https://raw.githubusercontent.com/StamusNetworks/Amsterdam/master/src/config/suricata/suricata.yaml -O /etc/suricata/suricata.yaml
+wget -q https://raw.githubusercontent.com/StamusNetworks/Amsterdam/master/src/config/suricata/threshold.config -O /etc/suricata/threshold.config
