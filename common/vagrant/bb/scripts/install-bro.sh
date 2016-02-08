77b1d318e484fc38861e618f10c8166218075a38
Hillar Aarelaid <hillar@aarelaid.net>
2016-02-08 02:08:38 +0200

    started bro

---
+++ common/vagrant/bb/scripts/install-bro.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+echo 'deb http://download.opensuse.org/repositories/network:/bro/xUbuntu_14.10/ /' >> /etc/apt/sources.list.d/bro.list
+sudo apt-get update
+sudo apt-get install bro
