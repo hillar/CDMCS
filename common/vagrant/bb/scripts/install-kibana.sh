e417815a0c9c31c95b16e9e7ac6b2b33ca9ef1d1
Hillar Aarelaid <hillar@aarelaid.net>
2016-02-08 21:10:08 +0200

    using temporary kibana4

--- common/vagrant/bb/scripts/install-kibana.sh
+++ common/vagrant/bb/scripts/install-kibana.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 echo "$0 got params: $1 $2 $3 $4"
+
+
+
+cd /tmp
+wget -q https://download.elastic.co/kibana/kibana/kibana-4.3.1-linux-x64.tar.gz
+cd /opt/
+tar -xzf /tmp/kibana-4.3.1-linux-x64.tar.gz
+ln -sf /opt/kibana-4.3.1-linux-x64 /opt/kibana
+/opt/kibana/bin/kibana plugin -i kibana/timelion
+#chown -R kibana.kibana /opt/kibana/optimize/
+/opt/kibana/bin/kibana > /var/log/kibana.log 2>&1 &
