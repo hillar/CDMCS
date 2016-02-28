66383d2287223bdc6b492420b499b8bc2d4e0b29
Hillar Aarelaid <hillar@aarelaid.net>
2016-02-29 00:33:59 +0200

    suricata day 2 added logstash install script

---
+++ suricata/day_2/boxes/scripts/install-logstash.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# this script:
+# 1) installs logstash
+# 2) sets elastic to $1 in conf.d/suricata.conf
+#
+
+if [ "$(id -u)" != "0" ]; then
+   echo "ERROR - This script must be run as root" 1>&2
+   exit 1
+fi
+
+ELASTIC=$(ifconfig eth0 2>/dev/null|grep 'inet addr'|cut -f2 -d':'|cut -f1 -d' ')
+echo 'deb http://packages.elasticsearch.org/logstash/2.2/debian stable main' > /etc/apt/sources.list.d/logstash.list
+apt-get update > /dev/null 2>&1
+apt-get -y --force-yes install logstash > /dev/null 2>&1
+#stealing amsterdam losgstash conf
+wget -4 -q https://raw.githubusercontent.com/StamusNetworks/Amsterdam/master/src/config/logstash/conf.d/logstash.conf -O /etc/logstash/conf.d/suricata.conf
+#    hosts => elasticsearch
+sed -i -e 's,hosts => elasticsearch,hosts => "'${ELASTIC}'"\n index => "logstash-%{+YYYY.MM.dd.HH}",g' /etc/logstash/conf.d/suricata.conf
+#fix this hack
+chmod 777 /var/log/suricata/eve.json
+service logstash start > /dev/null 2>&1
+sleep 2
+service logstash status
