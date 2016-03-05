fc11e1e7a3fde77499fea29cb50d3e4c844ec4e7
Hillar Aarelaid <hillar@aarelaid.net>
2016-03-05 21:55:48 +0200

    bro day intro dummy

---
+++ bro/day_intro/empty/scripts/dummy-install-bro.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# this script
+# 1) adds bro to atp sources
+# 2) uodates debs list
+# 3) installs bro from package
+
+echo 'deb http://download.opensuse.org/repositories/network:/bro/xUbuntu_14.04/ /' >> /etc/apt/sources.list.d/bro.list
+apt-get update
+apt-get -y --force-yes install bro
