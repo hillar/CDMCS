23eb7d117d9580b28544a4ebac8e7b23e2296ef6
Hillar Aarelaid <hillar@aarelaid.net>
2016-03-13 18:03:53 +0200

    first test run of bro install

--- suricata/day_2/boxes/scripts/install-bro-master-and-workers.sh
+++ suricata/day_2/boxes/scripts/install-bro-master-and-workers.sh
@@ -5,6 +5,16 @@
 #
 # ! depends on salt
 #
+# params :
+# $1 salt key pattern to match bro workers and proxyes
+# $2 workers ip list, sepatarated with comma
+# $3 proxyes ip list, sepatarated with comma
+
+echo "installing bro with:"
+echo "pattern: $1"
+echo "workers: $2"
+echo "proxyes: $3"
+echo "wait .."
 
 ## step-by-step (semi)manual version with salt
 ## taken from bash history, expect mistakes
@@ -25,3 +35,25 @@
 # <accept ssh fingerprints or set up .ssh/config to insecure/autoaccept>
 # salt '*' cmd.run 'setcap "CAP_NET_RAW+eip" /opt/bro/bin/bro'
 # su - bro -s /bin/bash -c '/opt/bro/bin/broctl install'
+
+
+# add bro repo and install
+echo "deb http://download.opensuse.org/repositories/network:/bro/xUbuntu_14.04/ /" >> /etc/apt/sources.list.d/bro.list
+wget http://download.opensuse.org/repositories/network:bro/xUbuntu_14.04/Release.key
+apt-key add - < Release.key
+apt-get update
+apt-get install bro
+#prepare for master setup
+useradd bro -N
+usermod -d /opt/bro bro
+mkdir /opt/bro/.ssh && chown -R bro:bro /opt/bro && chmod 750 /opt/bro/.ssh
+su - bro -s /bin/bash -c 'echo -en "\n\n"|ssh-keygen -t rsa -f /opt/bro/.ssh/id_rsa'
+#cat /opt/bro/.ssh/id_rsa.pub >> /opt/bro/.ssh/authorized_keys
+KEY=$(cat /opt/bro/.ssh/id_rsa.pub)
+salt "*$1*" cmd.run 'echo "$KEY" >> /opt/bro/.ssh/authorized_keys;chown -R bro:bro /opt/bro;'
+su - bro -s /bin/bash -c '/opt/bro/bin/broctl install'
+
+# test
+# USER='bro'
+# HOSTNAME="10.242.11.12"
+# su - bro -s /bin/bash -c 'scp ~/.ssh/id_rsa.pub $USER@$HOSTNAME:~/''
