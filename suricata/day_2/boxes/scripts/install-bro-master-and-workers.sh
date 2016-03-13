e304610307dba30e1257925046ce838573b62b90
Hillar Aarelaid <hillar@aarelaid.net>
2016-03-13 20:41:44 +0200

    moved proxys up

--- suricata/day_2/boxes/scripts/install-bro-master-and-workers.sh
+++ suricata/day_2/boxes/scripts/install-bro-master-and-workers.sh
@@ -8,8 +8,9 @@
 # params :
 # $1 manager ip
 # $2 salt key pattern to match bro workers and proxyes
-# $3 workers ip list, sepatarated with comma
-# $4 proxyes ip list, sepatarated with comma
+# $3 proxyes ip list, sepatarated with comma
+# $4 workers ip list, sepatarated with comma
+
 
 if [ "$(id -u)" != "0" ]; then
    echo "ERROR - This script must be run as root" 1>&2
@@ -19,14 +20,15 @@ fi
 echo "installing bro with:"
 echo "manager: $1"
 echo "pattern: $2"
-echo "workers: $3"
-echo "proxyes: $4"
+echo "proxyes: $3"
+echo "workers: $4"
 echo "wait .."
 
 MANAGER=$1
 BROS=$2
-WORKERS=$3
-PROXYS=$4
+PROXYS=$3
+WORKERS=$4
+
 
 # add bro repo and install
 echo "deb http://download.opensuse.org/repositories/network:/bro/xUbuntu_14.04/ /" >> /etc/apt/sources.list.d/bro.list
@@ -51,12 +53,11 @@ salt "*$BROS*" cmd.run 'adduser --system --home /opt/bro --no-create-home --ingr
 salt "*$BROS*" cmd.run 'chown -R bro:bro /opt/bro;'
 salt "*$BROS*" cmd.run 'echo "10.242.11.10 student-1-manager" >> /etc/hosts'
 # test
-
 echo "$WORKERS" | sed 's/,/\n/g'|while read IP;
 do
    su - bro -s /bin/bash -c 'ssh $IP'
 done
-
+# create node.cfg
 cat > /opt/bro/etc/node.cfg<<DELIM
 [manager]
 type=manager
