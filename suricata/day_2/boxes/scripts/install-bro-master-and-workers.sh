b7d9b4dca749190236ed42d234bbca79dfd63402
Hillar Aarelaid <hillar@aarelaid.net>
2016-03-13 20:38:41 +0200

    deleted temp comments

--- suricata/day_2/boxes/scripts/install-bro-master-and-workers.sh
+++ suricata/day_2/boxes/scripts/install-bro-master-and-workers.sh
@@ -28,29 +28,6 @@ BROS=$2
 WORKERS=$3
 PROXYS=$4
 
-BROS="student-1-bro-worker-b"
-
-## step-by-step (semi)manual version with salt
-## taken from bash history, expect mistakes
-## those commands are meant to be executed on saltmaster (also bro master)
-## salt master is also a minion to itself
-# salt '*' cmd.run 'echo "deb http://download.opensuse.org/repositories/network:/bro/xUbuntu_14.04/ /" >> /etc/apt/sources.list.d/bro.list'
-# salt '*' cmd.run 'wget http://download.opensuse.org/repositories/network:bro/xUbuntu_14.04/Release.key'
-# salt '*' cmd.run 'apt-key add - < Release.key'
-# salt '*' cmd.run 'apt-get update'
-# salt '*' pkg.install bro
-# salt '*' cmd.run 'useradd bro -N'
-# salt '*' cmd.run 'usermod -d /opt/bro bro'
-# salt '*' cmd.run 'mkdir /opt/bro/.ssh && chown -R bro:bro /opt/bro && chmod 750 /opt/bro/.ssh'
-# su - bro -s /bin/bash -c 'ssh-keygen -t rsa -f /opt/bro/.ssh/id_rsa'
-# salt '*' cmd.run "echo \"`cat /opt/bro/.ssh/id_rsa.pub`\" >> /opt/bro/.ssh/authorized_keys"
-# salt '*' cmd.run 'chown -R bro:bro /opt/bro'
-# <vim /opt/bro/etc/node.cfg>
-# <accept ssh fingerprints or set up .ssh/config to insecure/autoaccept>
-# salt '*' cmd.run 'setcap "CAP_NET_RAW+eip" /opt/bro/bin/bro'
-# su - bro -s /bin/bash -c '/opt/bro/bin/broctl install'
-
-
 # add bro repo and install
 echo "deb http://download.opensuse.org/repositories/network:/bro/xUbuntu_14.04/ /" >> /etc/apt/sources.list.d/bro.list
 wget -4 -q http://download.opensuse.org/repositories/network:bro/xUbuntu_14.04/Release.key
@@ -99,23 +76,5 @@ do
   echo "type=worker" >> /opt/bro/etc/node.cfg
   echo "host=$IP" >> /opt/bro/etc/node.cfg
 done
-
-#[manager]
-#type=manager
-#host=host1
-#
-#[proxy-1]
-#type=proxy
-#host=host1
-#
-#[worker-1]
-#type=worker
-#host=host2
-#interface=eth0
-#
-#[worker-2]
-#type=worker
-#host=host3
-#interface=eth0
 
 #su - bro -s /bin/bash -c '/opt/bro/bin/broctl deploy'
